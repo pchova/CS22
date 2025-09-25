8c3d78a7bdccbcc34bc9b84eab4d8ad0e2bc3e4d
pchova <p.chetnikova@gmail.com>
2025-09-24 21:47:09 -0700

    added pseudocode

--- CS22Wk1HW/CS22Wk1HW.py
+++ CS22Wk1HW/CS22Wk1HW.py
@@ -4,3 +4,20 @@
 # Assignment: Week 1 - Python Primer Part 1
 # Due Date: September 28, 2025
 ####################################################
+
+def main():
+    #open a file numbers.txt for writing
+    #file object should be named nums
+
+    #use a for loop and randint method to write
+    #10 numbers to the file , between 1-1000
+    #each num converted to a str, and have \n added to it
+
+    #close file
+
+    #open file again, but for reading
+    #create a function evensodds with 1 parameter for file
+
+
+#def evensodds(newfile):
+    #code here
