d7ab48d13fe0d2ff52866202c72870effb37f29c
pchova <p.chetnikova@gmail.com>
2025-09-24 21:40:14 -0700

    hw1 header

---
+++ CS22Wk1HW/CS22Wk1HW.py
@@ -0,0 +1,6 @@
+####################################################
+# CS 22, Prof. Muldrow
+# Name: Polina Chetnikova
+# Assignment: Week 1 - Python Primer Part 1
+# Due Date: September 28, 2025
+####################################################
