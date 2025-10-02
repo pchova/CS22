e95c990decc128a97e31f5bf235af4b4f28427b6
pchova <p.chetnikova@gmail.com>
2025-10-01 22:23:29 -0700

    initalized list with 5 zeros

---
+++ CS22Wk2HW/CS22Wk2HW.py
@@ -0,0 +1,13 @@
+####################################################
+# CS 22, Prof. Muldrow
+# Name: Polina Chetnikova
+# Assignment: Week 2 Homework Assignment
+# Due Date: October 5 2025
+####################################################
+
+def main():
+    nums_list = [0] * 5
+
+    print(nums_list)
+
+main()
