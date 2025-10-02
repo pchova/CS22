b671a2cf297796a304d812982b896bf9e8a26dca
pchova <p.chetnikova@gmail.com>
2025-10-01 22:28:03 -0700

    assign random int between 1-100 to each element in array

--- CS22Wk2HW/CS22Wk2HW.py
+++ CS22Wk2HW/CS22Wk2HW.py
@@ -4,10 +4,15 @@
 # Assignment: Week 2 Homework Assignment
 # Due Date: October 5 2025
 ####################################################
+import random
 
 def main():
+    #declare an array with 5 elements
     nums_list = [0] * 5
 
-    print(nums_list)
+    #assign a random value between 1-100 to each element in array
+    for i in range(len(nums_list)):
+        randNumber = random.randint(1,100)
+        nums_list[i] = randNumber
 
 main()
