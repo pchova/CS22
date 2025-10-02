03cb5c2efd810bc52950656b9044d49255ffe5a1
pchova <p.chetnikova@gmail.com>
2025-10-01 22:53:59 -0700

    created bigsmall() to return string with num > user input

--- CS22Wk2HW/CS22Wk2HW.py
+++ CS22Wk2HW/CS22Wk2HW.py
@@ -16,7 +16,27 @@ def main():
         nums_list[i] = randNumber
 
     #take user input and convert it to int variable
-    the_num = int(input("Enter a number: "))
-    print(type(the_num))
+    the_num = int(input("Enter an integer: "))
+    #print(type(the_num))
+
+    print("Here is your list: ", nums_list)
+    print(bigsmall(the_num, nums_list))
+
+def bigsmall(num, list):
+    i = 0
+    largerNums = []
+
+    #while count is less than length of the list, go through to find num > user input
+    while i < len(list):
+        if list[i] > num:
+            largerNums.append(list[i])
+        i += 1
+    
+    #convert largerNums list to str and join them with a single space
+    formatNums = " ".join(str(num) for num in largerNums)
+    
+    #return numbers greater than user input
+    return f"Numbers greater than {num}: {formatNums}"
+        
 
 main()
