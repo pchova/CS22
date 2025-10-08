fa555372db254ccf6376044f05d65121b1787006
pchova <p.chetnikova@gmail.com>
2025-10-07 21:35:27 -0700

    added header

---
+++ CS22Wk3HW/CS22Wk3HW.py
@@ -0,0 +1,6 @@
+####################################################
+# CS 22, Prof. Muldrow
+# Name: Polina Chetnikova
+# Assignment: Week 3 HW
+# Due Date: October 12 2025 
+####################################################
