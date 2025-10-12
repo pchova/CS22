ced7e9957722da9ba07f0740fc32af72692aeac1
pchova <p.chetnikova@gmail.com>
2025-10-12 15:21:48 -0700

    project 1 created athlete class

---
+++ CS22Project1/CS22Project1.py
@@ -0,0 +1,35 @@
+####################################################
+# CS 22, Prof. Muldrow
+# Name: Polina Chetnikova
+# Assignment: Project 1
+# Due Date: October 20 2025
+####################################################
+
+class Athlete:
+    def __init__(self, name, country, sport):
+        self.__name = name
+        self.__country = country
+        self.__sport = sport
+
+    #Setter Methods for Athlete class
+    def set_name(self, n):
+        self.__name = n
+    
+    def set_country(self, c):
+        self.__country = c
+    
+    def set_sport(self, s):
+        self.__sport = s
+
+    #Getter Methods for Athlete class
+    def get_name(self):
+        return self.__name
+    
+    def get_country(self):
+        return self.__country
+    
+    def set_sport(self):
+        return self.___sport
+    
+    
+    
