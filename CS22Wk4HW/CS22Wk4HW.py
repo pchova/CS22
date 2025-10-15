7d13975cb95b0540297639a96f11a421fee91d72
pchova <p.chetnikova@gmail.com>
2025-10-14 22:45:31 -0700

    added import statements and create 1D and 2D array

--- CS22Wk4HW/CS22Wk4HW.py
+++ CS22Wk4HW/CS22Wk4HW.py
@@ -4,3 +4,18 @@
 # Assignment: Week 4 HW
 # Due Date: October 19 2025 
 ####################################################
+import random
+import time
+import timeit
+
+def main():
+    #create a 1 dimensional array with 10 elements
+    list1 = [0] * 10
+    print(list1)
+
+    #create a 2 dimensional array with 10 rows and 10 columns
+    #list2 = [[0] * 10] * 10 - this is wrong! this creates a shallow copy!
+    list2 = [[0]*10 for i in range(10)]
+    print(list2)
+
+main()
