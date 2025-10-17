dac96b34fe01739d5bd550757fac322bd77ff2e9
pchova <p.chetnikova@gmail.com>
2025-10-16 20:29:58 -0700

    function set_list1D replaces each num in a list with a num between 1-500

--- CS22Wk4HW/CS22Wk4HW.py
+++ CS22Wk4HW/CS22Wk4HW.py
@@ -8,14 +8,23 @@ import random
 import time
 import timeit
 
+# Function set_list1D() accepts a list as a parameter
+# it replaces each item in the list with a random number between 1-500
+def set_list1D(list):
+    for item in range(0, len(list)):
+        list[item] = random.randint(0,500)
+    return list
+
+
 def main():
     #create a 1 dimensional array with 10 elements
     list1 = [0] * 10
     print(list1)
+    print(set_list1D(list1))
 
     #create a 2 dimensional array with 10 rows and 10 columns
     #list2 = [[0] * 10] * 10 - this is wrong! this creates a shallow copy!
-    list2 = [[0]*10 for i in range(10)]
-    print(list2)
+    #list2 = [[0] * 10 for i in range(10)]
+    #print(list2)
 
 main()
