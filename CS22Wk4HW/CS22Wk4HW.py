fb8b90575dcd693637c1ecaa628a961d7657a016
pchova <p.chetnikova@gmail.com>
2025-10-16 20:37:27 -0700

    created show_list1D to display all elements of list on 1 line

--- CS22Wk4HW/CS22Wk4HW.py
+++ CS22Wk4HW/CS22Wk4HW.py
@@ -8,19 +8,25 @@ import random
 import time
 import timeit
 
-# Function set_list1D() accepts a list as a parameter
-# it replaces each item in the list with a random number between 1-500
+# Function set_list1D() accepts a 1D array as a parameter
+# it replaces each item in the array with a random number between 1-500
 def set_list1D(list):
     for item in range(0, len(list)):
         list[item] = random.randint(0,500)
     return list
 
+# Function show_list1D() prints a 1D array on a single line
+def show_list1D(list):
+    for item in range(0, len(list)):
+        print(list[item], end=' ')
+    print()
 
 def main():
     #create a 1 dimensional array with 10 elements
     list1 = [0] * 10
     print(list1)
     print(set_list1D(list1))
+    show_list1D(list1)
 
     #create a 2 dimensional array with 10 rows and 10 columns
     #list2 = [[0] * 10] * 10 - this is wrong! this creates a shallow copy!
