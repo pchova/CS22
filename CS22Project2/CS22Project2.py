2277f9c229c7bcbac3a39472161c7e1b07a479f0
pchova <p.chetnikova@gmail.com>
2025-11-10 19:39:27 -0800

    circular linked lists project created

---
+++ CS22Project2/CS22Project2.py
@@ -0,0 +1,12 @@
+####################################################
+# CS 22, Prof. Muldrow
+# Name: Polina Chetnikova
+# Assignment: Project 2 Homework Assignment
+# Due Date: November 17 2025
+####################################################
+
+class Node:
+    def __init__(self, data):
+        self.__data = data
+        self.__next = None
+    
