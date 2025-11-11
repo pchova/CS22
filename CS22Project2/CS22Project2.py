9ae11841e0d5158b5407a3652c9456ab54ce5bd2
pchova <p.chetnikova@gmail.com>
2025-11-10 19:43:20 -0800

    set up function to insert node at the front of list

--- CS22Project2/CS22Project2.py
+++ CS22Project2/CS22Project2.py
@@ -10,3 +10,16 @@ class Node:
         self.__data = data
         self.__next = None
     
+class CircularLinkedList:
+    def __init__(self):
+        self.__head = None
+        self.__tail = None
+        self.__size = 0
+
+    def insert_at_head(self, data):
+        #creates a node using the parameter data
+        new_node = Node(data)
+        #places the node at the beginning of the list
+        #increases the size by 1
+    
+
