149f68b31105516e8502d1b1bb60c808f36726cd
pchova <p.chetnikova@gmail.com>
2025-11-13 20:32:11 -0800

    added insert_at_tail() function

--- CS22Project2/CS22Project2.py
+++ CS22Project2/CS22Project2.py
@@ -7,8 +7,8 @@
 
 class Node:
     def __init__(self, data):
-        self.__data = data
-        self.__next = None
+        self.data = data
+        self.next = None
     
 class CircularLinkedList:
     def __init__(self):
@@ -16,10 +16,70 @@ class CircularLinkedList:
         self.__tail = None
         self.__size = 0
 
+# Function insert_at_head() places a node at the beginning of the list
     def insert_at_head(self, data):
-        #creates a node using the parameter data
+        #creates a node using the parameter
         new_node = Node(data)
-        #places the node at the beginning of the list
+
+        #if head is none, both head and tail should be set to the new node
+        if self.__head == None:
+            self.__head = new_node
+            self.__tail = new_node
+            new_node.next = new_node
+        #else, the new node should be inserted at the head of the list, AND
+        #the tail's next field should point to head
+        else:
+            new_node.next = self.__head
+            self.__head = new_node
+            self.__tail.next = self.__head
+
         #increases the size by 1
+        self.__size += 1
     
+    def insert_at_tail(self, data):
+        #creates a node using the parameter 
+        new_node = Node(data)
+
+        if self.__head == None:
+            self.__head = new_node
+            self.__tail = new_node
+            new_node.next = new_node
+        else:
+            #old tail points to the new node
+            self.__tail.next = new_node
+            #new node points to head
+            new_node.next = self.__head
+            #tail should now be the new node
+            self.__tail = new_node
+
+        #increases the size by 1
+        self.__size += 1
+
+    def remove_from_head(self):
+        #remove a node at the beginning of the list
+
+        #decrease size by 1
+        self.__size -= 1
+
+        #return the data field of the node that was removed
+
+    def remove_from_tail(self):
+        #remove a node at the end of the list
+
+        #decrease size by 1
+        self.__size -= 1
+
+        #return the data field of the node that was removed
+
+    def display(self):
+        if self.__head == None:
+            print('List is empty')
+        else:
+            temp = self.__head
+            print('Here is your list:', end=' ')
+            while temp is not None:
+                print(temp.data, end=' ')
+            print()
+
+
 
